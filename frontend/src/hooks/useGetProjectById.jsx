f63bd9b316bb1c7a4e4d806eec197111bfeba628
AnandNandanK <anandwising@gmail.com>
2024-12-22 11:55:21 +0530

    sending token in usegetproject hook

--- frontend/src/hooks/useGetProjectById.jsx
+++ frontend/src/hooks/useGetProjectById.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { projectEndpoint } from '../services/apis';
 import { useEffect } from 'react';
 import axios from 'axios'
@@ -10,12 +10,16 @@ const {GET_PROJECT_BY_ID_API} = projectEndpoint;
 const UseGetProjectById = (projectId) => {
 
     const dispatch = useDispatch()
+    const { token } = useSelector(store => store.auth)
 
     useEffect(() => {
         const fetchProject= async () => {
             try {
                 const res = await axios.get(`${GET_PROJECT_BY_ID_API}/${projectId}`, {
-                    withCredentials: true
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
                 });
 
 
